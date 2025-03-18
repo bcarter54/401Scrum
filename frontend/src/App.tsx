de672c23b3be7f6e0362b1434f8777564f9fea03
mjpalm13 <mjpalm2006@gmail.com>
2025-03-18 00:17:36 -0600

    Home page with routes, images, and some styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,71 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Blessings from './Blessings';
-import Admin from './Admin';
+import Videos from './pages/VideosPage';
+// Uncomment these if you have the components for them
+/* import Testimony from './Testimony'; */
+/* import StudyGroups from './StudyGroups'; */
+/* import Faqs from './Faqs'; */
 
-function App() {
+const HomePage: React.FC = () => {
   return (
-    <>
-      <Router>
-        <nav>
-          <Link to="/">Home</Link> | <Link to="/new">New Page</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Blessings />} />
-          <Route path="/new" element={<Admin />} />
-        </Routes>
-      </Router>
-    </>
+    <div className="container">
+      {/* Welcome Section */}
+      <h1 className="welcome">Welcome</h1>
+
+      {/* Image of Christ */}
+      <div className="image-box">
+        <img src="/images/christ.jpg" alt="Christ" />
+      </div>
+
+      {/* Links Section */}
+      <div className="grid-container">
+        <Link to="/blessings" className="link-box">
+          <img src="/images/book-of-mormon.jpg" alt="Book of Mormon" />
+          <h2>Blessings</h2>
+        </Link>
+
+        <Link to="/testimony" className="link-box">
+          <img src="/images/testimony.jpg" alt="Testimony" />
+          <h2>Testimony</h2>
+        </Link>
+
+        <Link to="/study-groups" className="link-box">
+          <img src="/images/study-groups.jpg" alt="Study Groups" />
+          <h2>Study Groups</h2>
+        </Link>
+
+        <Link to="/videos" className="link-box">
+          <img src="/images/videos.jpg" alt="Videos" />
+          <h2>Videos</h2>
+        </Link>
+
+        <Link to="/faqs" className="link-box">
+          <img src="/images/faqs.jpg" alt="FAQs" />
+          <h2>FAQs</h2>
+        </Link>
+      </div>
+    </div>
   );
-}
+};
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* Default HomePage route */}
+        <Route path="/" element={<HomePage />} />
+
+        {/* Other Routes */}
+        <Route path="/blessings" element={<Blessings />} />
+        <Route path="/videos" element={<Videos />} />
+        {/* Uncomment these when you have the components ready */}
+        {/* <Route path="/testimony" element={<Testimony />} /> */}
+        {/* <Route path="/study-groups" element={<StudyGroups />} /> */}
+        {/* <Route path="/faqs" element={<Faqs />} /> */}
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
