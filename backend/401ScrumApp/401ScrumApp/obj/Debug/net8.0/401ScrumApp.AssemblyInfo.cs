f0d8cc50432d59ea7d52f6d95977e52450567f21
bcarter54 <carterbryce54@gmail.com>
2025-03-18 19:35:56 -0600

    background stuff

--- backend/401ScrumApp/401ScrumApp/obj/Debug/net8.0/401ScrumApp.AssemblyInfo.cs
+++ backend/401ScrumApp/401ScrumApp/obj/Debug/net8.0/401ScrumApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("401ScrumApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51b92f4e70f60089fd5b24d062b62e41eb367089")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6e52be6ee42a447eb4cbc5277670ef5f81820d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("401ScrumApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("401ScrumApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
