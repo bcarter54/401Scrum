db71588ac29c5327c2cdfe1ff87070cb51c6e187
dallenopenshaw <dallen.openshaw@gmail.com>
2025-03-24 07:47:32 -0600

    updating the branch

--- backend/401ScrumApp/401ScrumApp/obj/Debug/net8.0/401ScrumApp.AssemblyInfo.cs
+++ backend/401ScrumApp/401ScrumApp/obj/Debug/net8.0/401ScrumApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("401ScrumApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51b92f4e70f60089fd5b24d062b62e41eb367089")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+852e93170ea3a9a06ba0e4ae8addb7f32f0f28ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("401ScrumApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("401ScrumApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
